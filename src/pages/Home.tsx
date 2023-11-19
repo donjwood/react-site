92dddd6c742428384c3f5c54ffa37ff00542f571
Don Wood <donwood@gmail.com>
2023-11-19 01:02:52 -0600

    Added Contentful Client.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
 import { Container, Row, Col } from "react-bootstrap";
+import ContentfulClient from "../ContentfulClient";
 //import "../App.css";
 
 const HomePage: React.FC = () => {
   const [count, setCount] = useState(0);
 
-  console.log(import.meta.env.VITE_SOME_KEY);
-  console.log(import.meta.env.MODE);
+  const contentfulClient = new ContentfulClient()
+  
+  contentfulClient.getPage('About').then((foo => console.log(foo)));
 
   return (
     <Container fluid>
